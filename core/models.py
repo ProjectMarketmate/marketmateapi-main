fd2652c34d3c45e03af8e0303d1be0433e9135fa
asxxb <aseebshibin4@gmail.com>
2024-03-03 18:03:36 +0530

    space dedited

--- core/models.py
+++ core/models.py
@@ -32,8 +32,8 @@ class CartItem(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-###demo order
 
+###demo order
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
