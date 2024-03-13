bb0d1500c645c59a3d789eec0b9da06aa577229c
asxxb <aseebshibin4@gmail.com>
2024-03-13 19:13:30 +0530

    cart update and delete

--- core/api/app/urls.py
+++ core/api/app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from core.api.app.product import CategoryListAPIView, ProductListAPIView
-from core.api.app.cart import CartItemListApiView
+from core.api.app.cart import CartItemDeleteApiView, CartItemListApiView, CartItemUpdateApiView
 
 urlpatterns=[
   path('products/',ProductListAPIView.as_view(),name="app-products"),
   path('cart/',CartItemListApiView.as_view()),
-  
+  path('cart/update/<int:pk>/',CartItemUpdateApiView.as_view()),
+  path('cart/delete/<int:pk>/',CartItemDeleteApiView.as_view()),
   path('categories/',CategoryListAPIView.as_view()),
+  
 ]
