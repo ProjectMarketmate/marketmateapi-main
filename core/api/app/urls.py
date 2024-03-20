906ca480157c038685fe621f1d62ea44b05c16cc
asxxb <aseebshibin4@gmail.com>
2024-03-20 11:07:07 +0530

    order page

--- core/api/app/urls.py
+++ core/api/app/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import include, path
 
+from core.api.app.order import OrderApiView, OrderCreateApiView
 from core.api.app.product import CategoryListAPIView, ProductListAPIView
 from core.api.app.cart import CartItemDeleteApiView, CartItemListApiView, CartItemUpdateApiView
 
@@ -10,6 +11,7 @@ urlpatterns=[
   path('cart/update/<int:pk>/',CartItemUpdateApiView.as_view()),
   path('cart/delete/<int:pk>/',CartItemDeleteApiView.as_view()),
   path('categories/',CategoryListAPIView.as_view()),
- 
+  path('order/',OrderApiView.as_view(),name="app-order"),
+  path('order/create/',OrderCreateApiView.as_view(),name="app-order-create"),
   
 ]
--- core/models.py
+++ core/models.py
@@ -56,13 +56,17 @@ class Order(models.Model):
         CANCELED = 'Canceled', 'Canceled'
 
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=50, choices=ORDER_STATUS.choices, default=ORDER_STATUS.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Order #{self.pk} - {self.user.username}"
+    
 
-# class OrderItem(models.Model):
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     quantity = models.PositiveIntegerField()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
