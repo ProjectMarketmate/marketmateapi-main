ee2ba3255c5aca9ab1332d82e41bd3e411425413
asxxb <aseebshibin4@gmail.com>
2024-03-22 11:22:33 +0530

    order

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 
 urlpatterns=[
-  path('app/',include('core.api.app.urls'))
-
+  path('app/',include('core.api.app.urls')),
+  path('delivery/',include('core.api.delivery.urls'))
 ]
