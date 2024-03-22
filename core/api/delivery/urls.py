be82a3cf385618f6bec03ffb1af8830f082e49a9
asxxb <aseebshibin4@gmail.com>
2024-03-22 11:40:39 +0530

    ORDER

--- core/api/delivery/urls.py
+++ core/api/delivery/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from core.api.app.order import OrderRetrieveUpdateDestroyAPIView
 from core.api.delivery.orders import OrderListCreateAPIView
 
 
 
 urlpatterns=[
   path('orders/', OrderListCreateAPIView.as_view()),
+  path('orders/<int:pk>/', OrderRetrieveUpdateDestroyAPIView.as_view()),
   
 
 ]
