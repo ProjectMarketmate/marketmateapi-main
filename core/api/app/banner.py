4fc38f7bb04bcdc6256dd387d1f203f9f1ecfa55
asxxb <aseebshibin4@gmail.com>
2024-03-23 10:41:35 +0530

    banner

---
+++ core/api/app/banner.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from core.models import OfferBanner
+from rest_framework import generics
+
+
+class OfferBannerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OfferBanner
+        fields = '__all__'
+
+class OfferBannerListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = OfferBannerSerializer
+    queryset = OfferBanner.objects.all()
+
+class OfferBannerRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = OfferBannerSerializer
+    queryset = OfferBanner.objects.all()
+
