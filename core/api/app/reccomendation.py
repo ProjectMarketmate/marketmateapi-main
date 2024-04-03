1411b2aa3d4461acb03299e73f433a3408cefa3b
asxxb <aseebshibin4@gmail.com>
2024-04-03 09:57:21 +0530

    rec

--- core/api/app/reccomendation.py
+++ core/api/app/reccomendation.py
@@ -24,22 +24,15 @@ from django.http import JsonResponse
 from core.models import OrderItem, Product
 from collections import Counter
 from core.api.app.product import ProductSerializer  
+from django.db.models import Count
 
 def get_recommendations(request):
     user = request.user.id  # Retrieve the user's ID
+
+    # Get top products in OrderItem
+    top_products = OrderItem.objects.values('product').annotate(total=Count('product')).order_by('-total')[:3]
+    product_ids = [product['product'] for product in top_products]
+    recommendations = Product.objects.filter(id__in=product_ids).values('name','id')
+    serializer = ProductSerializer(recommendations, many=True)
     
-    # Get all orders of the user
-    user_orders = OrderItem.objects.filter(order__user_id=user)
-    
-    # Extract products from order items
-    products = [order_item.product for order_item in user_orders]
-    
-    # Count the frequency of each product
-    product_counts = Counter(products)
-    
-    # Sort products by frequency in descending order
-    sorted_products = sorted(product_counts.items(), key=lambda x: x[1], reverse=True)
-    
-    # Get top 3 recommended products for the customer
-    recommendations = [{'product': ProductSerializer(Product).data } for Product, count in sorted_products[:3]]
-    return JsonResponse({'recommendations': recommendations})
+    return JsonResponse({'recommendations': serializer.data})
